7ed27c7851025cf242a33c048499e4aaafd3fe81
Puttipong Tadang <puttipong@ookbee.com>
2022-03-01 00:33:50 +0700

    feat(app): add ui and card info

--- presentation/src/main/java/com/testanymind/presentation/view/activity/MainActivity.kt
+++ presentation/src/main/java/com/testanymind/presentation/view/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.testanymind.presentation.view.activity
 
-import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.chip.Chip
 import com.testanymind.presentation.R
 import com.testanymind.presentation.base.DataBindingActivity
 import com.testanymind.presentation.databinding.ActivityMainBinding
@@ -9,12 +11,42 @@ class MainActivity : DataBindingActivity<ActivityMainBinding>() {
 
     override fun layoutId() = R.layout.activity_main
 
-    override fun getToolBar(): Toolbar? {
-        TODO("Not yet implemented")
+    override fun getToolBar() = viewBinding.toolbar
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(androidx.core.R.menu.example_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun start() {
-        TODO("Not yet implemented")
+        title = ""
+
+        viewBinding.apply {
+
+        }
+        addChipToGroup("Android")
+        addChipToGroup("Kotlin")
+        addChipToGroup("Golang")
+        addChipToGroup("Retrofit")
+        addChipToGroup("Koin")
+        addChipToGroup("Jetpack")
     }
 
+    private fun addChipToGroup(tagText: String) {
+        val chip = Chip(this).apply {
+            text = tagText
+            isClickable = false
+            isCheckable = false
+            isCloseIconVisible = false
+        }
+        viewBinding.chipGroupSkill.addView(chip)
+    }
 }
